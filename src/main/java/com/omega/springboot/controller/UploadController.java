393bb3357c3a49936825a3230e197cd46719fb3b
KennySo <HoyanSo1996@163.com>
2024-10-12 19:40:16 +0800

    feat: 文件上传 Demo.

---
+++ src/main/java/com/omega/springboot/controller/UploadController.java
@@ -0,0 +1,68 @@
+package com.omega.springboot.controller;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Controller;
+import org.springframework.util.ResourceUtils;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.multipart.MultipartFile;
+
+import java.io.File;
+import java.io.IOException;
+
+/**
+ * Class UploadController
+ *
+ * @author KennySo
+ * @date 2024/10/12
+ */
+@Slf4j
+@Controller
+public class UploadController {
+
+    /**
+     * 导航到 upload.html
+     */
+    @RequestMapping("/upload.html")
+    public String uploadPage() {
+        return "upload";
+    }
+
+    /**
+     * 处理 upload 的数据
+     */
+    @PostMapping("/upload")
+    @ResponseBody
+    public String upload(@RequestParam("username") String username,
+                       @RequestParam("email") String email,
+                       @RequestParam("age") Integer age,
+                       @RequestParam("job") String job,
+                       @RequestParam("header") MultipartFile header,
+                       @RequestParam("pet_photos") MultipartFile[] petPhotos) throws IOException {
+        log.info("username = {}, email = {}, age = {}, job = {}", username, email, age, job);
+
+        String path = ResourceUtils.getURL("classpath:").getPath();
+        log.info("path = {}", path);
+        File file = new File(path  + "static/images/upload/");
+        if (!file.exists()) {
+            file.mkdirs();
+        }
+
+        if (!header.isEmpty()) {
+            String originalFilename = header.getOriginalFilename();
+            // 保存到动态创建的目录
+            header.transferTo(new File(file.getAbsoluteFile() + "/" + originalFilename));
+        }
+
+        for (MultipartFile petPhoto : petPhotos) {
+            if (!petPhoto.isEmpty()) {
+                String originalFilename = petPhoto.getOriginalFilename();
+                // 保存到动态创建的目录
+                petPhoto.transferTo(new File(file.getAbsoluteFile() + "/" + originalFilename));
+            }
+        }
+        return "upload successfully~";
+    }
+}
