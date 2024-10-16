fac2d56a6b0269d2f0da075b8461dece5b095ff6
KennySo <Hoyanso1996@163.com>
2024-10-16 15:36:41 +0800

    feat: 配置 WEB 组件（方式一）.

--- src/main/java/com/omega/springboot/MainApp.java
+++ src/main/java/com/omega/springboot/MainApp.java
@@ -5,7 +5,9 @@ import com.omega.springboot.entity.Monster2;
 import com.omega.springboot.entity.Monster3;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.servlet.ServletComponentScan;
 import org.springframework.context.ApplicationContext;
+import org.springframework.context.ConfigurableApplicationContext;
 import org.springframework.context.annotation.ComponentScan;
 
 
@@ -15,13 +17,14 @@ import org.springframework.context.annotation.ComponentScan;
  * @author KennySo
  * @date 2024/9/27
  */
+@ServletComponentScan("com.omega")
 @ComponentScan(value = "com.omega")
 @SpringBootApplication
 public class MainApp {
 
     // SpringBoot 程序启动入口
     public static void main(String[] args) {
-        ApplicationContext ioc = SpringApplication.run(MainApp.class, args);
+        ConfigurableApplicationContext ioc = SpringApplication.run(MainApp.class, args);
         // String[] beanDefinitionNames = ioc.getBeanDefinitionNames();
         // for (String beanDefinitionName : beanDefinitionNames) {
         //     // 搜索 OuterComponent 可以找到
@@ -30,11 +33,14 @@ public class MainApp {
 
         // System.out.println("IOC 容器中是否存在 dog : " + ioc.containsBean("dog"));
 
-        Monster monster = ioc.getBean( Monster.class);
-        Monster2 monster2 = ioc.getBean(Monster2.class);
-        Monster3 monster3 = ioc.getBean( Monster3.class);
-        System.out.println(monster);
-        System.out.println(monster2);
-        System.out.println(monster3);
+        // Monster monster = ioc.getBean( Monster.class);
+        // Monster2 monster2 = ioc.getBean(Monster2.class);
+        // Monster3 monster3 = ioc.getBean( Monster3.class);
+        // System.out.println(monster);
+        // System.out.println(monster2);
+        // System.out.println(monster3);
+
+        // 如果需要测试到 Filter 的 destroy() 和 Listener 的 contextDestroyed(), 就要放开 stop().
+        //ioc.stop();
     }
 }
---
+++ src/main/java/com/omega/springboot/servlet/Servlet_.java
@@ -0,0 +1,31 @@
+package com.omega.springboot.servlet;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+/**
+ * Class Servlet_
+ *
+ * @author KennySo
+ * @date 2024/10/16
+ */
+@WebServlet({"/servlet01", "/servlet02"})
+public class Servlet_ extends HttpServlet {
+
+    /**
+     * Tips: 请求进入原生 Servlet 后, 就不会再进入 DispatcherServlet 那一套流程汇总
+     */
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        resp.getWriter().write("hello, Servlet_!");
+    }
+
+    @Override
+    protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        this.doGet(req, resp);
+    }
+}
