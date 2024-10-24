68822a01f225592f537d472f3a40da9f13ba54f0
KennySo <HoyanSo1996@163.com>
2024-10-24 20:56:11 +0800

    feat: 使用配置类 配置tomcat属性.

---
+++ src/main/java/com/omega/springboot/config/CustomizationBean.java
@@ -0,0 +1,20 @@
+package com.omega.springboot.config;
+
+/**
+ * Class CustomizationBean
+ *
+ * @author KennySo
+ * @date 2024/10/24
+ */
+// @Configuration
+// public class CustomizationBean implements WebServerFactoryCustomizer<ConfigurableWebServerFactory> {
+//
+//     /**
+//      * 更多配置可以参考 EmbeddedWebServerFactoryCustomizerAutoConfiguration 中
+//      * 不同 WEB 容器的配置方式进行配置
+//      */
+//     @Override
+//     public void customize(ConfigurableWebServerFactory factory) {
+//         factory.setPort(9999); // 设置端口号
+//     }
+// }
